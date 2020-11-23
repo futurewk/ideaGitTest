8e16d429164e41e9583f36905b93bf3ac48e335b
weik <future_wk@aliyun.com>
2020-11-23 18:22:06 +0800

    增加了weight和height

--- gitModuleTest/src/com/weik/user.java
+++ gitModuleTest/src/com/weik/user.java
@@ -14,6 +14,22 @@ public class user {
     private int weight;
     private int height;
 
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
     public String getName() {
         return name;
     }
